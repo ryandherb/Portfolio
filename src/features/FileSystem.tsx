2457eec6a30043ae19240ead14befbfcdb802801
ryantrash <ryandherber@gmail.com>
2026-04-23 13:38:47 -0500

    Creating file system

---
+++ src/features/FileSystem.tsx
@@ -0,0 +1,55 @@
+import type { ReactNode } from "react";
+import * as Elements from "../constants/DocElements"
+
+type FileSystemNode = File | Directory;
+
+interface File {
+    type: 'file';
+    label: string;
+    content: ReactNode
+}
+
+interface Directory {
+    type: 'directory',
+    label: string;
+    children: FileSystemNode[];
+}
+
+export default class FileSystem {
+    home: Directory = {
+        type: 'directory',
+        label: 'home',
+        children: [
+            { type: 'file', label: 'resume.txt', content: Elements.resumeText },
+            {
+                type: 'directory', label: '/projects', children: [
+
+                ]
+            }
+        ]
+    }
+
+    dir: Directory;
+
+    constructor(){
+        this.dir = this.home;
+    }
+
+    cd = (file: string): string | null => {
+        if (file === "..") {
+            this.dir = this.home;
+            return "home";
+        }
+
+        for (const child of Object.values(this.dir.children)) {
+            if (child.label === file) {
+                if (child.type === 'directory') {
+                    this.dir = child;  
+                    console.log(this.dir);
+                    return file;
+                }
+            }
+        }
+        return null;
+    }
+}
