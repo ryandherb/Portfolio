ebf3b591979a836189613c7fd0ff2edb8b7ddf01
ryantrash <ryandherber@gmail.com>
2026-04-23 13:38:52 -0500

    Doc element file

---
+++ src/constants/DocElements.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+
+export const resumeText: ReactNode =
+    <>
+        This is my resume!
+        <h1>Things can be styled here!</h1>
+    </>
+
+export const youtubeText: ReactNode =
+    <>
+        My youtube channel!
+    </>
