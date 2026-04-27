bf2b6db864a08393e852ab85ef0db9859b7c35e6
ryantrash <ryandherber@gmail.com>
2026-04-27 11:33:38 -0500

    react router addition with new docview

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css'
 import Terminal from './components/Terminal';
-
+import DocViewer from './components/DocViewer';
 
 function App() {
 
   return (
-    <>
-      <Terminal />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Terminal/>} />
+        <Route path="/docView" element={<DocViewer/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
